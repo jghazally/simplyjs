bfe9c85872eaa7ac5c73078cd2eec22d000be22c
Jeffry Ghazally <jghazally@screamingcodemonkey.com>
2014-04-23 20:32:00 +1200

    Add in better display

--- simple.js
+++ simple.js
@@ -1,6 +1,10 @@
 var request = require('request');
 ajax({ url: 'http://www.geonet.org.nz/quakes/services/all.json' }, function(data){
 	data = JSON.parse(data);
-	var mag = data.features[0].properties.magnitude.toFixed(0);
+	data = data.features[0].properties;
+	var mag = data.magnitude.toFixed(1) + 'M',
+		intensity = data.intensity,
+		depth = data.depth.toFixed(2) + 'Metres';
 	simply.title(mag);
+	simply.text({ title: intensity, subtitle: 'Magnitude ' + mag, body:'Depth of ' + depth});
 });
